ea2a42a88910c010015dbb36346ce566f75e8652
Netanel Friedman <netanel.friedman6@gmail.com>
2026-09-05 14:44:33 +0300

    Add an original grid preserving grid

---
+++ whoops-core/src/grid/konst.rs
@@ -0,0 +1,71 @@
+use std::collections::HashSet;
+
+use super::Grid;
+use crate::pos::Pos;
+use crate::tile::Tile;
+
+/// disallows modification to known tiles at creation
+pub struct Konst<G> {
+    grid: G,
+    preserve: HashSet<Pos>,
+}
+
+impl<G> Konst<G>
+where
+    G: Grid,
+{
+    pub fn new(grid: G) -> Self {
+        let preserve = grid
+            .iter()
+            .filter_map(|(pos, tile)| (tile.is_wall() || tile.is_dot()).then_some(pos))
+            .collect();
+
+        Self { grid, preserve }
+    }
+}
+
+impl<G> Grid for Konst<G>
+where
+    G: Grid,
+{
+    fn get(&self, pos: Pos) -> Option<Tile> {
+        self.grid.get(pos)
+    }
+
+    fn set(&mut self, pos: Pos, tile: Tile) -> Option<Tile> {
+        if self.preserve.contains(&pos) {
+            return None;
+        }
+
+        self.grid.set(pos, tile)
+    }
+
+    fn width(&self) -> u32 {
+        self.grid.width()
+    }
+
+    fn height(&self) -> u32 {
+        self.grid.height()
+    }
+}
+
+impl<G> From<G> for Konst<G>
+where
+    G: Grid,
+{
+    fn from(value: G) -> Self {
+        Self::new(value)
+    }
+}
+
+impl<G> AsMut<G> for Konst<G> {
+    fn as_mut(&mut self) -> &mut G {
+        &mut self.grid
+    }
+}
+
+impl<G> AsRef<G> for Konst<G> {
+    fn as_ref(&self) -> &G {
+        &self.grid
+    }
+}
