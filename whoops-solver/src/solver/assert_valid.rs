9d1c9e67fde35d540d6168714e7bf9d9a6abcd8c
Netanel Friedman <netanel.friedman6@gmail.com>
2026-09-10 15:10:03 +0300

    Fix the `AssertValid` solver

--- whoops-solver/src/solver/assert_valid.rs
+++ whoops-solver/src/solver/assert_valid.rs
@@ -1,5 +1,6 @@
 use whoops_core::grid::GridIter;
 use whoops_core::offset::Offset;
+use whoops_core::tile::Tile;
 
 use crate::Solver;
 
@@ -12,22 +13,35 @@ where
 {
     fn solve(&mut self, grid: G) -> Result<G, G> {
         // make sure no value tile sees more than expected
-        let is_valid = grid
-            .iter()
-            .filter_map(|(pos, tile)| Some(pos).zip(tile.as_value()))
-            .all(|(pos, value)| {
-                let total: u8 = Offset::DIRECTIONS
-                    .into_iter()
-                    .map(|direction| {
-                        grid.iter_tile_offset(pos, direction)
-                            .skip(1)
-                            .take_while(|tile| tile.is_dot())
-                            .count() as u8
-                    })
-                    .sum();
+        let is_valid = grid.iter().all(|(pos, tile)| {
+            match tile {
+                // wall tiles and unknown tiles are always valid
+                Tile::Wall | Tile::Unknown => true,
 
-                total <= value
-            });
+                // if there's no value (a user-placed dot), make sure that it may see at least one
+                // other dot - at least one immediate neighbor is not a wall
+                Tile::Dot(0) => Offset::DIRECTIONS
+                    .into_iter()
+                    .filter_map(|off| pos.add_offset(off))
+                    .filter_map(|pos| grid.get(pos))
+                    .any(|tile| !tile.is_wall()),
+
+                // if there's a value, make sure that the dot doesn't see more than its value
+                Tile::Dot(value) => {
+                    let total_visible: u8 = Offset::DIRECTIONS
+                        .into_iter()
+                        .map(|direction| {
+                            grid.iter_tile_offset(pos, direction)
+                                .skip(1)
+                                .take_while(|tile| tile.is_dot())
+                                .count() as u8
+                        })
+                        .sum();
+
+                    value >= total_visible
+                }
+            }
+        });
 
         if is_valid { Ok(grid) } else { Err(grid) }
     }
